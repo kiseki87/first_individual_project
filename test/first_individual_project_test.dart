ea1a8769dff24bf777c8669f6333fba03fbbe73e
kiseki87 <ycs223388@gmail.com>
2025-09-25 22:35:40 +0900

    디렉토리 및 파일 분리

---
+++ test/first_individual_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_individual_project/first_individual_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
